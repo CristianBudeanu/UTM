fbde46537e99c331fc62b8c3f4e2f3070be942e2
Cristian Budeanu <budeanu.cristian28@gmail.com>
2022-04-23 09:53:25 +0300

    Update!

--- SDA/lab5/lab_5.c
+++ SDA/lab5/lab_5.c
@@ -29,20 +29,32 @@ void free_array(int *arr, int size)
     free(arr);
 }
 
-void insertion_sort(int *arr, int size)
+void insertion_sort()
 {
-  int i;
+  int i,j,size=100;
   int curr, pred;
-  for (i = 1; i < size; i++)
+  int * arr;
+  clock_t begin = clock();
+  for(j=0;j<4;j++)
   {
-    curr = arr[i];
-    pred = i - 1;
+    arr = create_init_array(size);
+    printf("[Insertion_Sort]\nFor %d elements-\n",size);
+    for (i = 1; i < size; i++)
+    {
+      curr = arr[i];
+      pred = i - 1;
 
-    while (curr < arr[pred] && pred >= 0) {
-      arr[pred + 1] = arr[pred];
-      --pred;
+      while (curr < arr[pred] && pred >= 0) {
+        arr[pred + 1] = arr[pred];
+        --pred;
+      }
+      arr[pred + 1] = curr;
     }
-    arr[pred + 1] = curr;
+    clock_t end = clock();
+    float time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
+    printf("The elapsed time is %f seconds\n", time_spent);
+    size = (size*100)/10;
+    free_array(arr,size);
   }
 }
 
@@ -50,20 +62,11 @@ int main()
 {
   int i, size=100;
   int *arr;
-  printf("----------Start------------\n");
-  for(i=0;i<4;i++)
-  {
-    printf("-[Insertion_Sort]\nFor %d elements-\n",size);
-    arr = create_init_array(size);
-    clock_t begin = clock();
+  printf("-----------Start------------\n\n");
+
     insertion_sort(arr,size);
-    clock_t end = clock();
-    float time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
-    printf("The elapsed time is %f seconds\n", time_spent);
-    //print_array(arr,size);
-    //printf("\n-----------------------------------------------------------\n");
-    size = (size*100)/10;
-  }
+
+
 
 
 
