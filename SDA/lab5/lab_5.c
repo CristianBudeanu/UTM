ce34df50e754638dc63557e3db284e1d0174876f
Cristian Budeanu <budeanu.cristian28@gmail.com>
2022-04-25 11:43:11 +0300

    Added Menu !

--- SDA/lab5/lab_5.c
+++ SDA/lab5/lab_5.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <conio.h>
 
 int *create_init_array(int size)
 {
@@ -20,24 +21,15 @@ void print_array(int *arr, int size)
   int i;
   for(i=0;i<size;i++)
   {
-    printf("|%d",arr[i]);
+    printf("|%d\t",arr[i]);
   }
 }
 
-void free_array(int *arr, int size)
-{
-    free(arr);
-}
 
-void insertion_sort()
+void insertion_sort(int *arr,int size)
 {
-  int i,j,size=100;
+  int i,j;
   int curr, pred;
-  int * arr;
-  for(j=0;j<4;j++)
-  {
-    clock_t begin = clock();
-    arr = create_init_array(size);
     printf("[Insertion_Sort]\nFor %d elements-\n",size);
     for (i = 1; i < size; i++)
     {
@@ -50,13 +42,7 @@ void insertion_sort()
       }
       arr[pred + 1] = curr;
     }
-    clock_t end = clock();
-    float time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
-    printf("The elapsed time is %f seconds\n", time_spent);
-    size = (size*100)/10;
-    free_array(arr,size);
   }
-}
 
 
 
@@ -112,19 +98,129 @@ arr = create_init_array(size);
   float time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
   printf("The elapsed time is %f seconds\n", time_spent);
   size = (size*100)/10;
-  free_array(arr,size);
+  free(arr);
   }
 }
 
+
+int menu()
+{
+    int error_input_found = 0;
+menu_start:
+    system("cls");
+    printf("%+206s\n","|                       Menu                       |\n");
+    printf("%+205s\n","|       ------------------------------------       |");
+    printf("%+205s\n","|                   1.init_array                   |");
+    printf("%+205s\n","|                   2.print_array                  |");
+		printf("%+205s\n","|                   3.free_arr                     |");
+    printf("%+205s\n","|                   4.insertion_sort               |");
+    printf("%+205s\n","|                   5.heap_sort                    |");
+    printf("%+205s\n","|                   0.exit                         |");
+    int command = 5;
+    if(error_input_found)
+    {
+        printf("\n\nVa rog sa selectati o comanda existenta.\n");
+    }
+    printf("\n\nCommand : ");
+    scanf("%d",&command);
+    while(command>5 || command<0)
+    {
+      printf("Command does not exist .\n");
+      error_input_found = 1;
+      scanf("%*[^\n]");
+      goto menu_start;
+    }
+    return command;
+}
 
 int main()
 {
   printf("-----------Start------------\n\n");
 
-    insertion_sort();
     printf("\n\n\n");
-    heapSort();
+    //heapSort();
 
+    int size;
+    int command = 5;
+    int *array = NULL;
+    int choose[4] = {100,1000,10000,100000};
+    while(command)
+	  {
+	    command = menu();
+			if(command == 1)
+			{
+				if(array != NULL)
+				{
+					free(array);
+				}
+choose_again:
+					printf("Choose : \n1) 100\n2) 1000\n3) 10000\n4) 100000\n\n");
+          printf("Size[1,2,3,4]: ");
+					scanf("%d",&size);
+          for(int i=1;i<=4;i++)
+          {
+            if(i == size)
+            {
+              array = create_init_array(choose[i-1]);
+              break;
+            }
+          }
+          if(size>4 && size<0)
+          {
+            goto choose_again;
+          }
+			}
+      else if(array != NULL)
+			{
+	    switch(command)
+	    {
+	      case 2:
+	      {
+					print_array(array,choose[size-1]);
+	        break;
+	      }
+	      case 3:
+	      {
+          if(array!=NULL)
+          {
+            free(array);
+            printf("Tabloul a fost eliberat.\n\n");
+            array = NULL;
+          }
+	        break;
+	      }
+
+	      case 4:
+	      {
+          clock_t begin = clock();
+          insertion_sort(array,choose[size-1]);
+          clock_t end = clock();
+          float time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
+          printf("The elapsed time is %f seconds\n", time_spent);
+
+	        break;
+	      }
+	      case 5:
+	      {
+          clock_t begin = clock();
+          insertion_sort(array,choose[size-1]);
+          clock_t end = clock();
+          float time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
+          printf("The elapsed time is %f seconds\n", time_spent);
+	        break;
+	      }
+	      case 0:
+	      {
+	        return 0;
+	      }
+	    }
+		} else
+		{
+			printf("\nArray is NULL! Init please! \n");
+		}
+	    printf("\n\nPress any Key to continue!\n");
+	    _getch();
+	  }
 
 
 
